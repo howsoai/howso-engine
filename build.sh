db27ffad8c7bd13403408b13411aa1f51b6822df
Andrew Bassett <43486400+apbassett@users.noreply.github.com>
2024-07-27 23:09:25 -0400

    21024: Adds --debug-internal-memory flag to tests

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ test() {
   echo "Running howso-engine unit tests with amalgam version \"${amlg_version}\"..."
   ${amlg_exe} ./ut_comprehensive_unit_test.amlg | tee /tmp/ut_results
   reset_version_file
-  local ut_res=$(cat /tmp/ut_results | grep "PASSED : Total comprehensive test execution time" | wc -l)
+  local ut_res=$(cat /tmp/ut_results --debug-internal-memory | grep "PASSED : Total comprehensive test execution time" | wc -l)
   if [ $ut_res \< 1 ]; then
     cat /tmp/ut_results
     exit 81
