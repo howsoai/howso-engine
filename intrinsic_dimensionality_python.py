06ab2f28df859b5555320a0a4fdd5226e271ae88
Jacob Beel <54082169+jdbeel@users.noreply.github.com>
2024-11-01 13:16:13 -0400

    Begin adapting for react/react_aggregate.

--- intrinsic_dimensionality_python.py
+++ intrinsic_dimensionality_python.py
@@ -17,37 +17,42 @@ for feature in nom_features:
 t = Trainee(features=features)
 t.train(df)
 t.analyze()
-print("Actual num features               : ", len(features))
-print("Adjusted num features             : ", num_features)
 
-estimate_id_response = t.client.execute(
-    t.id,
-    "estimate_intrinsic_dimensionality",
-    {
-        "num_samples": 50,
-        # "features": list(df.columns),
-        # "weight_feature": None,
-    }
-)
-print("Estimated intrinsic dimensionality: ", round(estimate_id_response["d"], 3))
+print(t.react_aggregate(
+    details={"intrinsic_dimensionality": True}
+))
 
-estimate_local_fill_response = t.client.execute(
-    t.id,
-    "estimate_local_fill",
-    {
-        "num_samples": 50,
-        "d": estimate_id_response["d"]
-    }
-)
-print(
-    "Should use generate_new_cases       : ",
-    estimate_local_fill_response["new_case_generation_recommended"]
-)
-print(
-    "  Avg. new-case volume              : ",
-    estimate_local_fill_response["avg_new_case_volume"]
-)
-print(
-    "  Avg. k volume                     : ",
-    estimate_local_fill_response["avg_k_volume"]
-)
+# print("Actual num features               : ", len(features))
+# print("Adjusted num features             : ", num_features)
+
+# estimate_id_response = t.client.execute(
+#     t.id,
+#     "estimate_intrinsic_dimensionality",
+#     {
+#         "num_samples": 50,
+#         # "features": list(df.columns),
+#         # "weight_feature": None,
+#     }
+# )
+# print("Estimated intrinsic dimensionality: ", round(estimate_id_response["d"], 3))
+
+# estimate_local_fill_response = t.client.execute(
+#     t.id,
+#     "estimate_local_fill",
+#     {
+#         "num_samples": 50,
+#         "d": estimate_id_response["d"]
+#     }
+# )
+# print(
+#     "Should use generate_new_cases       : ",
+#     estimate_local_fill_response["new_case_generation_recommended"]
+# )
+# print(
+#     "  Avg. new-case volume              : ",
+#     estimate_local_fill_response["avg_new_case_volume"]
+# )
+# print(
+#     "  Avg. k volume                     : ",
+#     estimate_local_fill_response["avg_k_volume"]
+# )
